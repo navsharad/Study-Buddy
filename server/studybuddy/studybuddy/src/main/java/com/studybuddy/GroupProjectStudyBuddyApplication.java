483c23d554fcf5f12bd9be2adb3377b365eaf2cc
wen1225 <kenho12251999@gmail.com>
2021-04-24 16:51:57 -0700

    changed java dependency to 16

---
+++ server/studybuddy/studybuddy/src/main/java/com/studybuddy/GroupProjectStudyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.studybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectStudyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectStudyBuddyApplication.class, args);
+	}
+
+}
