0269c30dae98c7d1adbfbbfbb10f931e11e50e72
Kevin <k.vinnguy3n@gmail.com>
2021-04-24 18:28:33 -0700

    Kev's first commit, personal folder

---
+++ server/KevStudyBuddy/src/test/java/com/example/StudyBuddy/StudyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudyBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
